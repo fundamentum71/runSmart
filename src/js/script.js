ecba4f6674286ba467a391348e473341ce1557e0
Aleksandr <it.kucherov@gmail.com>
2022-03-29 22:32:04 +0300

    add functional tabs

--- src/js/script.js
+++ src/js/script.js
@@ -1,23 +1,3 @@
-//!slick-slider
-//$(document).ready(function () {
-//	$('.carousel__inner').slick({
-//		speed: 1200,
-//		//adaptiveHeight: true,
-//		prevArrow:
-//			'<button type="button" class="slick-prev"><img src="../icons/left.svg"></button>',
-//		nextArrow:
-//			'<button type="button" class="slick-next"><img src="../icons/right.svg"></button>',
-//		responsive: [
-//			{
-//				breakpoint: 992,
-//				settings: {
-//					dots: true,
-//					arrows: false,
-//				},
-//			},
-//		],
-//	});
-//});
 //!tini-slider
 const slider = tns({
 	container: '.carousel__inner',
@@ -44,4 +24,38 @@ document.querySelector('.prev').addEventListener('click', function () {
 });
 document.querySelector('.next').addEventListener('click', function () {
 	slider.goTo('next');
+});
+
+//!tabs
+
+$(document).ready(function () {
+	$('ul.catalog__tabs').on(
+		'click',
+		'li:not(.catalog__tab_active)',
+		function () {
+			$(this)
+				.addClass('catalog__tab_active')
+				.siblings()
+				.removeClass('catalog__tab_active')
+				.closest('div.container')
+				.find('div.catalog__content')
+				.removeClass('catalog__content_active')
+				.eq($(this).index())
+				.addClass('catalog__content_active');
+		}
+	);
+
+	function toggleSlaid(item) {
+		$(item).each(function (i) {
+			$(this).on('click', function (e) {
+				e.preventDefault();
+				$('.catalog-item__content')
+					.eq(i)
+					.toggleClass('catalog-item__content_active');
+				$('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+			});
+		});
+	}
+	toggleSlaid('.catalog-item__link');
+	toggleSlaid('.catalog-item__back');
 });

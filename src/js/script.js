71c6c9c992d4943f7a7b09a9cef213248dcc75e5
Aleksandr <it.kucherov@gmail.com>
2022-03-31 16:01:31 +0300

    add validation and maskPhone

--- src/js/script.js
+++ src/js/script.js
@@ -75,4 +75,39 @@ $(document).ready(function () {
 			$('.overlay, #order').fadeIn('fast');
 		});
 	});
+
+	function validationForms(form) {
+		$(form).validate({
+			rules: {
+				name: {
+					required: true,
+					minlength: 2,
+				},
+				phone: 'required',
+				email: {
+					required: true,
+					email: true,
+				},
+			},
+			messages: {
+				name: {
+					required: 'Пожалуйста, введите свое имя',
+					minlength: jQuery.validator.format('введите {0} символа!'),
+				},
+				phone: 'Пожалуйста, введите свой номер телефона',
+				email: {
+					required: 'Пожалуйста, введите свою почту',
+					email: 'Неправильно введен адрес почты',
+				},
+			},
+		});
+	}
+
+	validationForms('#consultation-form');
+	validationForms('#consultation form');
+	validationForms('#order form');
+
+	//phone mask
+
+	$('input[name=phone]').mask('+7(999) 999-99-99');
 });
